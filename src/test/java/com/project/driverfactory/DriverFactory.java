d85f5313b89e8c58a299f7fe9e851a66e3a9a366
bharath6369 <panyalabharath.reddy@walkingtree.tech>
2023-11-02 14:44:12 +0530

    changes related to driver factory

--- src/test/java/com/project/driverfactory/DriverFactory.java
+++ src/test/java/com/project/driverfactory/DriverFactory.java
@@ -6,9 +6,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -22,7 +24,6 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import com.project.utils.CommonUtils;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -38,7 +39,7 @@ public class DriverFactory {
 			  DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName("chrome");
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
+            options.addArguments("--headless=new");
             driver = WebDriverManager.chromedriver().capabilities(options).create();
 			  // driver= WebDriverManager.chromedriver().create();
 			
@@ -59,52 +60,9 @@ public class DriverFactory {
 		}
 		
 		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
-		
-		return driver;
-		
-	}
-	
-	public static WebDriver getDriver() {
-		
-		return driver;
-		
-	}
-	
-
-}
-
-
-public class DriverFactory {
-	
-	static WebDriver driver = null;
-	private static Logger logger = LogManager.getLogger(DriverFactory.class);
-	
-	public static WebDriver initializeBrowser(String browserName) {
-		
-		if(browserName.equals("chrome")) {
-			
-			driver = new ChromeDriver();
-			logger.info("chrome driver initialized");
-			
-		}else if(browserName.equals("firefox")) {
-			
-			driver = new FirefoxDriver();
-			logger.info("firefox  driver initialized");
-		}else if(browserName.equals("edge")) {
-			
-			driver = new EdgeDriver();
-			logger.info("Edge driver initialized");
-		}else if(browserName.equals("safari")) {
-			
-			driver = new SafariDriver();
-			logger.info("safari driver initialized");
-		}
-		
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
+		// driver.manage().window().maximize();
+		driver.manage().window().setPosition(new org.openqa.selenium.Point(0, 0)); 
+		driver.manage().window().setSize(new Dimension(1920, 1080));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
 		
