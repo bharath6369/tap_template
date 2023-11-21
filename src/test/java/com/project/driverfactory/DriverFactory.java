fe6f2747528c8c0ad2578e17b87885fca3a5bc59
Bharath <109291386+bharath6369@users.noreply.github.com>
2023-11-21 15:29:25 +0530

    Update DriverFactory.java

--- src/test/java/com/project/driverfactory/DriverFactory.java
+++ src/test/java/com/project/driverfactory/DriverFactory.java
@@ -28,53 +28,43 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.safari.SafariDriver;
 
 
-public class DriverFactory {
-	
-	public static WebDriver driver = null;
-	private static Logger logger = LogManager.getLogger(DriverFactory.class);
-	
-	public static WebDriver initializeBrowser(String browserName) {
-		
-		if(browserName.equals("chrome")) {
-			  DesiredCapabilities capabilities = new DesiredCapabilities();
+public class DriverFactory  {
+
+    public  static WebDriver driver ;
+    private static Logger logger = LogManager.getLogger(DriverFactory.class);
+    public static void initializeBrowser(String browserName)  {
+
+        if (browserName.equals("chrome")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName("chrome");
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless=new");
             driver = WebDriverManager.chromedriver().capabilities(options).create();
-			  // driver= WebDriverManager.chromedriver().create();
-			
-			// logger.info("chrome driver initialized");
-			
-		}else if(browserName.equals("firefox")) {
-			
-			driver = new FirefoxDriver();
-			logger.info("firefox  driver initialized");
-		}else if(browserName.equals("edge")) {
-			
-			driver = new EdgeDriver();
-			logger.info("Edge driver initialized");
-		}else if(browserName.equals("safari")) {
-			
-			driver = new SafariDriver();
-			logger.info("safari driver initialized");
-		}
-		
-		driver.manage().deleteAllCookies();
-		// driver.manage().window().maximize();
-		driver.manage().window().setPosition(new org.openqa.selenium.Point(0, 0)); 
-		driver.manage().window().setSize(new Dimension(1920, 1080));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
-		
-		return driver;
-		
-	}
-	
-	public static WebDriver getDriver() {
-		
-		return driver;
-		
-	}
-	
+//            driver= WebDriverManager.chromedriver().create();
+            logger.info("chrome driver initialized");
+        } else if (browserName.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/main/drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+            logger.info("firefox  driver initialized");
 
+        } else if (browserName.equals("edge")) {
+            driver = new EdgeDriver();
+            logger.info("Edge driver initialized");
+
+        } else if (browserName.equals("safari")) {
+            driver = new SafariDriver();
+            logger.info("safari driver initialized");
+        }
+
+        driver.manage().deleteAllCookies();
+        // driver.manage().window().maximize();
+        driver.manage().window().setPosition(new Point(0, 0)); driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+
+    }
 }
